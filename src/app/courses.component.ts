0b18bfc45baaa5c78667c168f29043b9463c4a2f
Robyn MacDonald <Robyn.MacDonald@vu.com>
2020-07-02 14:31:06 -0500

    directives and insert list of courses

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,8 +3,22 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'courses', // quick css:  <courses> selector is "courses"  or <div class="courses" ".courses" or <div id="courses" "#courses"
-    template: '<h2>Courses</h2>'
+    //backtick means we can break up into multiple lines?
+    template: ` 
+        <h2>{{ "Title: " + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+     ` 
 })
 export class CoursesComponent{
+    title = "List of Courses";
 
+    getTitle(){
+        return this.title;
+    }
+
+    courses = ["course1", "course2", "course3"];
 }
