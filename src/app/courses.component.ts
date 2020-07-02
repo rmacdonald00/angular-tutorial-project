2999a2b543fdd6d3b72428e6669230168ebb39cc
Robyn MacDonald <Robyn.MacDonald@vu.com>
2020-07-01 19:53:35 -0500

    initial commit

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,6 +1,10 @@
 
 import {Component} from '@angular/core';
 
+@Component({
+    selector: 'courses', // quick css:  <courses> selector is "courses"  or <div class="courses" ".courses" or <div id="courses" "#courses"
+    template: '<h2>Courses</h2>'
+})
 export class CoursesComponent{
 
 }
